b5a17965d4a053a1b6671f5b2c0fb9fffa8d328e
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-09-28 20:22:48 +0530

    Turned into sections

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser,AiOutlineBook} from 'react-icons/ai'
+import {RiServiceFill} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
 
+
+
+RiServiceFill
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <AiOutlineBook />
+      </a>
+      <a href="#services">
+        <RiServiceFill />
+      </a>
+      <a href="#contact">
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
   )
 }
 

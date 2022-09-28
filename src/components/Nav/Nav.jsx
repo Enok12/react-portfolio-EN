8f2746dd3ba013df351fd31efcad3e8097b268e5
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-09-28 20:39:57 +0530

    Navigation Toggle Fix

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser,AiOutlineBook} from 'react-icons/ai'
 import {RiServiceFill} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 
-
-
-RiServiceFill
-
 const Nav = () => {
+
+  const [activeNav,setactiveNav] = useState('#')
+
   return (
     <nav>
-      <a href="#" className='active'>
+      <a href="#" className={activeNav === '#' ? 'active':''}
+      onClick={() => setactiveNav('#')}>
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a href="#about" className={activeNav === '#about' ? 'active':''}
+      onClick={() => setactiveNav('#about')}>
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a href="#experience" className={activeNav === '#experience' ? 'active':''}
+      onClick={() => setactiveNav('#experience')}>
         <AiOutlineBook />
       </a>
-      <a href="#services">
+      <a href="#services" className={activeNav === '#services' ? 'active':''}
+      onClick={() => setactiveNav('#services')}>
         <RiServiceFill />
       </a>
-      <a href="#contact">
+      <a href="#contact" className={activeNav === '#contact' ? 'active':''}
+      onClick={() => setactiveNav('#contact')}>
         <BiMessageSquareDetail />
       </a>
     </nav>

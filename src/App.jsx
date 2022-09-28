7e92cf32c0a562aaa918952ead220f3372bd9493
unknown <enokdilshan115@gmail.com>
2022-09-28 14:12:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Experience from './components/Experience/Experience'
+import Portfolio from './components/Portfolio/Portfolio'
+import Testimonials from './components/Testimonials/Testimonial'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+
+import './index.css'
+
+function App() {
+
+  return (
+     <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+
+     </>
+  )
+}
+
+export default App
